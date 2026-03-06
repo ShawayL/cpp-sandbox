d2e17ee9db48787d6d2a72d15a3dda6c2b7a06d3
ShawayL <741077606@qq.com>
2026-03-06 16:48:35 +0800

    添加 CMake 选项以支持安装依赖项，并在项目设置中标记为高级选项

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -1,3 +1,5 @@
+include(CMakeDependentOption)
+
 macro(cpp_sandbox_setup_options)
   if(NOT PROJECT_IS_TOP_LEVEL)
     option(CPP_SANDBOX_BUILD_TESTS "Build unit tests" OFF)
@@ -6,6 +8,12 @@ macro(cpp_sandbox_setup_options)
     option(CPP_SANDBOX_BUILD_TESTS "Build unit tests" ON)
     option(CPP_SANDBOX_INSTALL "Enable install targets" ON)
   endif()
+  cmake_dependent_option(CPP_SANDBOX_INSTALL_DEPS
+    "Enable install dependencies"
+    OFF
+    "CPP_SANDBOX_INSTALL"
+    OFF
+  )
 
   option(CPP_SANDBOX_USE_CPM "Use CPM to setup dependencies" ON)
   option(CPP_SANDBOX_BUILD_WITH_GDAL "Build with gdal" OFF)
@@ -13,6 +21,7 @@ macro(cpp_sandbox_setup_options)
   if(NOT PROJECT_IS_TOP_LEVEL)
     mark_as_advanced(CPP_SANDBOX_BUILD_TESTS
       CPP_SANDBOX_INSTALL
+      CPP_SANDBOX_INSTALL_DEPS
       CPP_SANDBOX_USE_CPM
       CPP_SANDBOX_BUILD_WITH_GDAL
     )
